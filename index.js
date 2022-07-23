cda2fbdf3d2f5284f91afb5663cb851080665147
LuckyShmucky <christianluciano567@gmail.com>
2022-07-23 11:05:34 -0700

    fixed import statements

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require(express)
-const app = express()
-// const mongoose = require(mongoose)
-const cors = require(cors)
 require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
 // mongoose.connect()
 
 
 
 app.options('*', cors())
 app.use(express.json())
-// app.use(express.static('public'))
-// app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.status(200).json({
