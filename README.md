90baa48e280cf4827282d84da017466f18bf7212
LuckyShmucky <christianluciano567@gmail.com>
2022-07-23 10:59:48 -0700

    first push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # House-Calls-BE
+
+House Calls is an application for people who would like access to a doctor but don't want to leave the house. This is the Back end app for House Call service.
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require(express)
+const app = express()
+// const mongoose = require(mongoose)
+const cors = require(cors)
+require('dotenv').config()
+// mongoose.connect()
+
+
+
+app.options('*', cors())
+app.use(express.json())
+// app.use(express.static('public'))
+// app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the home route for House Calls'
+    })
+})
+
+app.get('*', (req, res) =>{
+    res.status(404).json({
+        message: "This url doesn't exist"
+    })
+})
+
+app.listen(process.env.PORT, () =>{
+    console.log('server is running')
+})
