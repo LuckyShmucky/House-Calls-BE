1d783ec7c538d1f10dbf8c26b59b5298a33345f4
LuckyShmucky <christianluciano567@gmail.com>
2022-07-23 13:17:33 -0700

    posting to the db successfully

--- src/controllers/patientsController.js
+++ src/controllers/patientsController.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Patient = require('../models/patient')
+
 
 router.get('/', (req, res) => {
     res.status(200).json({
@@ -10,15 +12,14 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
     console.log(req.body)
     if (req.body === false) return;
-
     try{
-        // const newPatient = await 
+        const newPatient = await Patient.create(req.body)
         res.status(200).json({
-            message: `${req.body}`
+            message: `${newPatient} was created`
         })
     } catch(err){
         res.status(400).json({
-            message: `${err}`
+            message: `${err} occured`
         })
     }
 })
