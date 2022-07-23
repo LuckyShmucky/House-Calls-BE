8fa54bbed46924b572b18dbeafa8566dfd86b066
LuckyShmucky <christianluciano567@gmail.com>
2022-07-23 11:14:41 -0700

    added notes about using cors in index.js

--- index.js
+++ index.js
@@ -27,3 +27,22 @@ app.get('*', (req, res) =>{
 app.listen(process.env.PORT, () =>{
     console.log('server is running')
 })
+
+
+
+/// For later use when we want the server to be a little more secure ///
+// cors allows us to specifiy what urls can make a request to our server, by doing so below: //
+
+// here we are declaring a variable and setting a property called origin to the url of a website we want to allow to make requests to the backend
+// and another property as a status message
+// let corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+   
+// in the get route method we pass in the cors function as a parameter (which has the Cors option parameter declared early as a parameter)
+// basically this tells the get method that only this website has access to this route
+//   app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for only example.com.'})
+//   })
+   
