2bdca0a7a4c6b150a885d1aa15684eef4e6e61f9
Craig Caparaz <cappuccino1890@Craigs-Air.lan1>
2022-07-27 20:27:41 -0700

    chat

---
+++ src/models/chat.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+mongoose.connect(process.env.MONGO_STRING);
+
+const chatSchema = new Schema(
+    {
+        id: { serialNumber: String, primary: 'Key' },
+        author: { type: String, required: true },
+        content: { type: String, required: true},
+        interlocutor: {type: String, required: true }
+
+    },
+    {toJSON: { virtuals: true }}
+);
+
+const chat = mongoose.model("medicalProvider", chatSchema);
+
+module.exports = chat;
